b04853d7da942401b8aeb7e6e4fa0c46221d7507
luisbaquiax <luis.baquiax1995@gmail.com>
2024-12-06 22:43:55 -0600

    add project

---
+++ FortranPeg/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import parseInput from "$lib/parser";
+    import CodeMirror from "svelte-codemirror-editor"
+
+    let editorText = $state("");
+    let result = $state("");
+    
+    function parse() {
+        result = parseInput(editorText.trim());
+    }
+</script>
+
+<div class="p-5 flex flex-row h-full max-h-full">
+    <div class="h-full w-1/2 mx-2 rounded-md border-black border overflow-y-scroll">
+        <CodeMirror bind:value={editorText} on:change={parse}/>
+    </div>
+    <div class="rounded-md border-black border-2 h-full w-1/2 mx-2">
+        <p class="text-red-500">
+            {result}
+        </p>
+    </div>
+</div>
